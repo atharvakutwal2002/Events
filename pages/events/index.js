05361aa81d21f5560414d1fbf09024769c6f1612
Atharva <atharvakutwal2002@gmail.com>
2022-10-19 20:44:56 +0530

    header configured

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,11 @@
+import { getAllEvents } from "../../dummy-data"
+import EventList from '../../components/events/event-list'
+
+
 function AllEventsPage(){
+    const events= getAllEvents();
     return <div>
-        <h1>All events</h1>
+        <EventList items={events}/>
     </div>
 }
 
