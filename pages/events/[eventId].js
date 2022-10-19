ac2d1eeeb73435d3adb08b9c48137f3c4ae13c3c
Atharva <atharvakutwal2002@gmail.com>
2022-10-19 22:17:07 +0530

    Polished

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,26 +1,36 @@
-import {useRouter} from 'next/router'
-import { Fragment } from 'react';
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics  from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content'
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
-function  EventDetailPage(){
-    const router = useRouter();
-    const eventId=router.query.eventId
-    const event =getEventById(eventId);
-    if(!event){
-        return <p>No event found !</p>
-    }
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
     return (
-        <Fragment>
-            <EventSummary title={event.title}/>
-            <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
-            <EventContent>
-                <p>{event.description}</p>
-            </EventContent>
-        </Fragment>
-    )
+      <ErrorAlert>
+        <p>No event found !</p>
+      </ErrorAlert>
+    );
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-export default EventDetailPage
+export default EventDetailPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,20 @@
 import { getAllEvents } from "../../dummy-data"
 import EventList from '../../components/events/event-list'
-
+import EventSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 function AllEventsPage(){
     const events= getAllEvents();
-    return <div>
+    const router=useRouter();
+    function findEventsHandler(year , month) {
+        const fullPath=`/events/${year}/${month}`
+        router.push(fullPath);
+    }
+    return <Fragment>
+        <EventSearch onSearch={findEventsHandler}/>
         <EventList items={events}/>
-    </div>
+    </Fragment>
 }
 
 export default AllEventsPage
