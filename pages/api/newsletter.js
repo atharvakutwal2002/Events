71709e4de57ec9789374f5a946092daee089f7d4
Atharva <atharvakutwal2002@gmail.com>
2022-10-25 17:08:32 +0530

    input files and api routes configured

---
+++ pages/api/newsletter.js
@@ -0,0 +1,14 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address" });
+      return;
+    }
+    console.log(userEmail);
+    res.status(201).json({ message: "Signed Up" });
+  }
+}
+
+export default handler;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,7 @@ import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 import Head from 'next/head'
+import Comments from "../../components/input/comments";
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -34,6 +35,7 @@ function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id}/>
     </Fragment>
   );
 }
